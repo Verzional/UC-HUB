f28a021d96fc09ce26d45404780359a5bcf4fb1a
Verzional <verzional777@gmail.com>
2026-01-07 22:59:35 +0700

    SurveyController & Register Reroute to Survey & Placeholder Form

--- database/migrations/2026_01_07_154806_create_surveys_table.php
+++ database/migrations/2026_01_07_154806_create_surveys_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('primary_interest');
             $table->decimal('cgpa', 3, 2);
-
             $table->timestamps();
         });
     }
